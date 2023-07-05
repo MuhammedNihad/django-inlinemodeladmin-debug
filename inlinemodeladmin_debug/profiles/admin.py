19d58e06e0a8c9b665e4293f5fc0029a4f39fca7
Muhammed Nihad <muhammednihad999@gmail.com>
2023-07-06 00:28:03 +0530

    register retailer in admin with inlines to reproduce the issue
    Register Retailer model in admin with StackedInline to reproduce the below stackoverflow question
    https://stackoverflow.com/q/76620687/12042940

--- inlinemodeladmin_debug/profiles/admin.py
+++ inlinemodeladmin_debug/profiles/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Retailer
+
+
+class UserInline(admin.StackedInline):
+    model = User
+    fields = ["username", "password1", "password2"]
+    fk_name = "user"
+
+
+@admin.register(Retailer)
+class RetailerAdmin(admin.ModelAdmin):
+    inlines = [UserInline]
+    fieldsets = (
+        (
+            None,
+            {"fields": ("user", "phone", "address", "google_map")},
+        ),
+    )
+    list_display = [
+        "user",
+        "phone",
+    ]
