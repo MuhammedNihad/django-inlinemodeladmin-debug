dda13cba877563719b410c6f375857354571ab2f
Muhammed Nihad <muhammednihad999@gmail.com>
2023-07-06 00:01:46 +0530

    feat: add retailer model to keep additional user details

--- inlinemodeladmin_debug/profiles/models.py
+++ inlinemodeladmin_debug/profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Retailer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="retailer")
+    phone = models.PositiveBigIntegerField(default=0, blank=True)
+    address = models.TextField(max_length=512, default="", blank=True)
+    google_map = models.URLField(max_length=1024, default="", blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
