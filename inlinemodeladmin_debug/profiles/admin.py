07af904021102e76781814411d73ee76468ace2c
Muhammed Nihad <muhammednihad999@gmail.com>
2023-07-14 22:44:28 +0530

    fix: hide delete checkbox on Retailer object change page
    Apparently, we cannot set can_delete=False as an attribute in RetailerAdmin class like in django default inline admin classes.
    As well as we cannot pass can_delete in inline_reverse as form and field passes. Which causes UserCreationForm fails to validate
    when we tick the delete checkbox and hit save button in retailer admin page without entering new passwords in both password and
    password confirmation fields (similar to when creating a new user and which can be inconvenient in this scenario).
    
    So, I overridden the `ReverseInlineModelAdmin` class to set can_delete attribute to False to hide delete checkbox in Retailer object change page

--- inlinemodeladmin_debug/profiles/admin.py
+++ inlinemodeladmin_debug/profiles/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.forms import UserCreationForm
-from django_reverse_admin import ReverseModelAdmin
+from django_reverse_admin import ReverseModelAdmin, ReverseInlineModelAdmin
 
 from .models import Retailer
+
+
+class CustomReverseInlineModelAdmin(ReverseInlineModelAdmin):
+    """    
+    By overriding ReverseInlineModelAdmin and setting can_delete to False in the formset, the delete
+    checkbox is hidden, preventing the deletion of the Retailer object and its parent object in the User
+    model when using the UserCreationForm as the form in inline_reverse attribute of RetailerAdmin class.
+   
+    Otherwise, If an attempt is made to delete the retailer model object by checking the checkbox and
+    hitting save button without providing a new password in both the password and password confirmation fields
+    (similar to when creating new user, which can be inconvenient in this case), a validation ValueError will
+    be raised by UserCreationForm with the message "The User could not be changed because the data didn't validate".
+    """
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        formset.can_delete = False
+        return formset
 
 
 @admin.register(Retailer)
@@ -21,3 +39,13 @@ class RetailerAdmin(ReverseModelAdmin):
         "user",
         "phone",
     ]
+
+    def get_inline_instances(self, request, obj=None):
+        """
+        Overrides the get_inline_instances method to use CustomReverseInlineModelAdmin class for reverse inline instances.
+        """
+        inline_instances = super().get_inline_instances(request, obj)
+        for inline in inline_instances:
+            if isinstance(inline, ReverseInlineModelAdmin):
+                inline.__class__ = CustomReverseInlineModelAdmin
+        return inline_instances
